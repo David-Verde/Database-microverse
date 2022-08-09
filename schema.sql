d7f7712cb014ccedd6ff164d33897f2220d3cef8
Unyielding1 <david.verde.alvarez@gmail.com>
2022-08-09 16:35:07 -0500

    2milestone

--- schema.sql
+++ schema.sql
@@ -9,3 +9,8 @@ CREATE TABLE animals (
     weight_kg decimal
 );
 SELECT * FROM animals;
+
+-- Add a column species of type string to your animals table. Modify your schema.sql file.
+
+ALTER TABLE animals 
+    ADD species VARCHAR(70);
